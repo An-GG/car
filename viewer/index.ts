942c4800fcf00e5ea915e4ba95cb6fd4ca4c2493
Ankush Girotra <ankush.girotra@gmail.com>
2021-07-27 13:58:27 -0500

    oop delete nm

--- viewer/index.ts
+++ viewer/index.ts
@@ -1,6 +1,9 @@
 import cv = require('./canvas');
+import SerialPort = require('serialport');
 
 exports = {};
 cv.onDraw((c:CanvasRenderingContext2D) => {
     c.fillRect(10,10,100,100);
 })
+
+let a = new SerialPort()
