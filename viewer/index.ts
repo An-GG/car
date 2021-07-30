59227d82c797d1c5d906392dd275e89542b76d21
Ankush Girotra <ankush.girotra@gmail.com>
2021-07-30 08:18:14 -0500

    sync

--- viewer/index.ts
+++ viewer/index.ts
@@ -1,14 +1,56 @@
-import cv = require('./canvas');
-import cp = require('child_process');
+import * as cv from './canvas';
+import { ChildProcess, spawn } from 'child_process';
+import { stdout } from 'process';
 
 
-cv.ActivateCanvas(window);
-cv.onDraw((c) => { c.fillRect(0,0,window.innerWidth,window.innerHeight);});
+
+export let SHARED:SharedData = {
+    can_frames: [],
+}
+
+export function init() {
+    cv.ActivateCanvas(window);
+    cv.onDraw(draw);
+   
+//
+    let serial = spawn('node', ['./serial_worker.js']);
+    SHARED.serial_worker = serial
+    serial.stdout.on('data', (data) => {
+       // Could've sent multiple lines at once
+        stdout.write(data.toString(), 'utf8', ()=>{});
+        console.log(data.toString());
+    });
+}
 
 
-let serial = cp.spawn('node', ['./serial_worker.js']);
-serial.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+export function deinit() {
+    SHARED.serial_worker?.kill();        
+}
 
 
+function draw(c:CanvasRenderingContext2D) {
+    c.fillRect(0, 0, window.innerWidth, window.innerHeight);    
+}
+
+function addFrame(f:CANFrame) {
+    
+}
+
+init();
+
+
+
+
+// TYPINGS
+
+type SharedData = {
+    can_frames: CANFrame[],
+    serial_worker?: ChildProcess
+}
+
+type CANFrame = {
+    str:string,
+    dev_id:number,
+    time:number,
+    data_b16:string
+}
